a9593c3653de03c08b78d546486a76e06038f69f
yefek <261905495+yefek@users.noreply.github.com>
2026-05-29 19:54:44 +0300

    Polish mobile WishlistAI navigation layering

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - offline support without trapping users on stale HTML
-const CACHE_NAME = 'wishlist-v16';
+const CACHE_NAME = 'wishlist-v17';
 const ASSETS = [
     './',
     './index.html',
