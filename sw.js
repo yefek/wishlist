cb80df3d84b22dd538078f3490ec7e311cd50aa2
yefek <261905495+yefek@users.noreply.github.com>
2026-05-30 16:20:24 +0300

    Lower mobile tab bar

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - offline support without trapping users on stale HTML
-const CACHE_NAME = 'wishlist-v21';
+const CACHE_NAME = 'wishlist-v22';
 const ASSETS = [
     './',
     './index.html',
